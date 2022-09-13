16ba4a7bcb631891816ed9dcc2003f68fdb0eb19
Techy2001 <74059089+techy2001@users.noreply.github.com>
2022-09-13 09:47:11 +0100

    feat: repo start

---
+++ GD2-OOP/src/main/java/com/dkit/gd2/corlett/App.java
@@ -0,0 +1,13 @@
+package com.dkit.gd2.corlett;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
